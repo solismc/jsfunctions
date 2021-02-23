4fce6ee968fff7f1f30c5eb876e820c13731ffb1
solismc <manolocsolis@gmail.com>
2021-02-23 15:38:15 -0500

    Added a function example adding 2 numbers

--- public/main.js
+++ public/main.js
@@ -13,3 +13,9 @@
  }
  let message = greetings();
  console.log(message); // this should log out Hello
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+let result = sum(2, 3);
+console.log(result); // result should show 5
