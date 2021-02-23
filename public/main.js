08049ec6fe9271761514db760ce15f1f8895f038
solismc <manolocsolis@gmail.com>
2021-02-23 15:27:31 -0500

    Greeting Function example

--- public/main.js
+++ public/main.js
@@ -1,9 +1,15 @@
 // Functions
 
-function printAll() {
+/*function printAll() {
   for(let i = 0; i < arguments.length; i++) {
     console.log(arguments[i]);
   }
 }
  printAll(1, 2, 3, 4, 5);
- printAll(10, 20);
+ printAll(10, 20);*/
+
+ function greeting() {
+   console.log('Hello');
+ }
+
+ greeting(); //this should log out Hello
