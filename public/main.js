a9a9f3bf41a719db9f58355daafe39c82ad360a7
solismc <manolocsolis@gmail.com>
2021-02-25 13:23:42 -0500

    Added a Closure Counter Example

--- public/main.js
+++ public/main.js
@@ -40,3 +40,14 @@ let greeting = (function() {
   };
 })();
 console.log(greeting.getMessage());
+
+function setupCounter( val ) {
+  return function (counter) {
+    return val++;
+  }
+}
+let counter1 = setupCounter(0);
+console.log(counter1()); // 0
+console.log(counter1()); // 1
+let counter2 = setupCounter(10);
+console.log(counter2());
